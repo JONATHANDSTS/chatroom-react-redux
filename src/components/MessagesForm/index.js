bd4edb2707c2c08d7cc5139bbef3e2613eb96b36
JONATHANDSTS <jonath850@gmail.com>
2021-03-17 20:49:00 +0100

    add button

--- src/components/MessagesForm/index.js
+++ src/components/MessagesForm/index.js
@@ -5,6 +5,7 @@ import './style.scss';
 const MessageForm = () => (
   <form className="message-form">
     <input className="form-input" type="text" placeholder="saisissez un message" />
+    <button type="submit" className="form-submit">&gt;</button>
   </form>
 );
 
