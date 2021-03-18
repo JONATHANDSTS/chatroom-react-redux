65ef0666af0bf7711b38ca3a4883ebec091c1760
JONATHANDSTS <jonath850@gmail.com>
2021-03-18 11:08:16 +0100

    add reducer to have a state to use

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import counter from './counter';
+import messages from './message';
 
 export default combineReducers({
-  counter,
+  messages,
 });
---
+++ src/reducers/message.js
@@ -0,0 +1,29 @@
+const stateInitial = {
+  list: [
+    {
+      id: '1',
+      author: 'Karadoc',
+      content: 'combiem de noisettes dans ce saussison',
+    },
+    {
+      id: '2',
+      author: 'Perceval',
+      content: 'ce qui compte cest les valeurs',
+    },
+    {
+      id: '3',
+      author: 'Arthur',
+      content: 'mais quelle truffe',
+    },
+  ],
+  messageText: 'je suis en cours decriture',
+};
+
+const reducerMessage = (state = stateInitial, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducerMessage;
